2df70f7183f98bba92484c755afe5bee3ff791a1
cwiggin9 <cwiggins.g@gmail.com>
2024-06-19 22:12:57 -0800

    Created ProductPage component.

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,16 @@
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 
 function Card(props) {
+
+  let navigate = useNavigate(); 
+  const routeChange = () => {
+    let path = `/`; 
+    navigate(path);
+  }
+
   return (
-    <div className='Card'>
+    <div className='Card' onClick={routeChange}>
       <img src="" alt={props.name} />
     </div>
   );
